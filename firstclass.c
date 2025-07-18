4d766efaf8a2c3d7f5106bb8ed23ed8bda511539
Inthas1409 <ice.inthas14@gmail.com>
2025-07-18 10:25:17 +0000

    Lab1

---
+++ firstclass.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello World!\n",21);
+    printf("Welcome to Robotics and AI\n!");
+    return (0);
+}
---
+++ lab-1/ex02.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is : %s\n","Inthas Songsawang");
+    printf("I am %d years old.\n",19);
+    printf("I am %d cm tall\n",174);
+    return (0);
+}
